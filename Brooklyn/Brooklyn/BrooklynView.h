c442d6a9d2aac26789eecf2d9faa43d9ad9909b4
Pedro Carrasco <pedrommcarrasco@gmail.com>
2018-10-30 14:21:44 +0000

    Created Project

---
+++ Brooklyn/Brooklyn/BrooklynView.h
@@ -0,0 +1,13 @@
+//
+//  BrooklynView.h
+//  Brooklyn
+//
+//  Created by Pedro Carrasco on 30/10/2018.
+//  Copyright © 2018 Pedro Carrasco. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BrooklynView : ScreenSaverView
+
+@end
---
+++ Brooklyn/Brooklyn/BrooklynView.m
@@ -0,0 +1,52 @@
+//
+//  BrooklynView.m
+//  Brooklyn
+//
+//  Created by Pedro Carrasco on 30/10/2018.
+//  Copyright © 2018 Pedro Carrasco. All rights reserved.
+//
+
+#import "BrooklynView.h"
+
+@implementation BrooklynView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
