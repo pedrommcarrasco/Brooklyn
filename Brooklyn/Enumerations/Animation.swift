8eb448474c3aad3461ad0f000f1016a6763500a5
Pedro Carrasco <pedrommcarrasco@gmail.com>
2019-03-01 23:58:37 +0000

    Added Animations: City Life, Color Path, Fruit Ninja, Golden Barbs, Pink & Purple, Popcorns, Sulley’s Fur

--- Brooklyn/Enumerations/Animation.swift
+++ Brooklyn/Enumerations/Animation.swift
@@ -15,14 +15,18 @@ enum Animation: String, CaseIterable {
     case ballPit
     case blueSand
     case cells
+    case cityLife
+    case colorPath
     case connectivity
     case cubicMess
     case defragmentation
     case eggsy
     case fadingPieces
     case fangs
+    case fruitNinja
     case geometricShapes
     case glow
+    case goldenBarbs
     case gummyWorms
     case hills
     case iris
@@ -36,7 +40,9 @@ enum Animation: String, CaseIterable {
     case noiseStripes
     case paintbrush
     case picasso
+    case pinkAndPurple
     case playDoh
+    case popcorns
     case riverNoir
     case runningInGrass
     case shards
@@ -44,6 +50,7 @@ enum Animation: String, CaseIterable {
     case snakes
     case soundSpectrum
     case splash
+    case sulleysFur
     case surfaceWater
     case trapezium
     case unstablePipes
@@ -66,14 +73,18 @@ extension Animation {
         case .ballPit: return "Ball Pit"
         case .blueSand: return "Blue Sand"
         case .cells: return "Cells"
+        case .cityLife: return "City Life"
+        case .colorPath: return "Color Path"
         case .connectivity: return "Connectivity"
         case .cubicMess: return "Cubic Mess"
         case .defragmentation: return "Defragmentation"
         case .eggsy: return "Eggsy"
         case .fadingPieces: return "Fading Pieces"
         case .fangs: return "Fangs"
+        case .fruitNinja: return "Fruit Ninja"
         case .geometricShapes: return "Geometric Shapes"
         case .glow: return "Glow"
+        case .goldenBarbs: return "Golden Barbs"
         case .gummyWorms: return "Gummy Worms"
         case .hills: return "Hills"
         case .iris: return "Iris"
@@ -87,7 +98,9 @@ extension Animation {
         case .noiseStripes: return "Noise Stripes"
         case .paintbrush: return "Paintbrush"
         case .picasso: return "Picasso"
+        case .pinkAndPurple: return "Pink & Purple"
         case .playDoh: return "Play-Doh"
+        case .popcorns: return "Popcorns"
         case .riverNoir: return "River Noir"
         case .runningInGrass: return "Running in Grass"
         case .shards: return "Shards"
@@ -95,6 +108,7 @@ extension Animation {
         case .snakes: return "Snakes"
         case .soundSpectrum: return "Sound Spectrum"
         case .splash: return "Splash"
+        case .sulleysFur: return "Sulley's Fur"
         case .surfaceWater: return "Surface Water"
         case .trapezium: return "Trapezium"
         case .unstablePipes: return "Unstable Pipes"
